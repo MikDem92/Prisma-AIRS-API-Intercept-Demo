0f37669dfac8bbf3d8357fb483eb63de8709150b
MikDem92 <m.demikhovskiy@gmail.com>
2025-11-08 13:49:39 +0100

    fix: Another jest config fix

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,18 @@
 const config = {
     // Use the 'js' extension for module resolution
     moduleFileExtensions: ['js'],
+    
     // Look for test files ending in .test.js
     testMatch: ['**/src/**/*.test.js'],
+    
     // Set test environment to node
     testEnvironment: 'node',
-    
-    // CRITICAL FIX: Explicitly tell Jest to treat .js files as ESM.
-    // This resolves the "require is not defined" error when running in a Node ESM project.
-    extensionsToTreatAsEsm: ['.js'],
+  
+    // CRITICAL FIX: Instruct Jest to transform the uuid package
+    transformIgnorePatterns: [
+      // This regex ignores all of node_modules EXCEPT 'uuid'
+      '/node_modules/(?!uuid)/',
+    ],
   };
   
   export default config;
